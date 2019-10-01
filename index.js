7aa236ee98586e34d0b4f446f79a5da14ef063b6
Michael Evans <mmevansjr@gmail.com>
2019-10-01 07:14:16 -0500

    done

--- index.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('input');
-
-function addingEventListener() {
-    input.addEventListener('click', function(event) {
-        alert('I was clicked!');
-    });
-}
